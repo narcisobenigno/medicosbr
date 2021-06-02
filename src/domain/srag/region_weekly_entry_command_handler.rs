cc0f3d14220b207b6eacceec5ea71d1fb8b24a78
Narciso Benigno <narciso.benigno@indebted.co>
2021-06-03 00:09:22 +1000

    refactor: organize how to use create

--- src/domain/srag/region_weekly_entry_command_handler.rs
+++ src/domain/srag/region_weekly_entry_command_handler.rs
@@ -1,15 +1,18 @@
-use super::vo;
-use super::vo::{Case, Region, TotalReported, YearWeek};
 use crate::common;
 use crate::common::es;
-use crate::common::es::AggregateId;
-use crate::domain::RegionalWeeklyReport;
+use crate::domain;
+use crate::domain::srag::vo;
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 use uuid::Uuid;
 
 #[derive(PartialEq, Debug, Serialize, Deserialize)]
-pub struct RegionWeeklyEventDetected(pub Region, pub Case, pub TotalReported, pub YearWeek);
+pub struct RegionWeeklyEventDetected(
+    pub vo::Region,
+    pub vo::Case,
+    pub vo::TotalReported,
+    pub vo::YearWeek,
+);
 
 impl es::Payload for RegionWeeklyEventDetected {
     type UnmarshalErr = ();
@@ -47,7 +50,7 @@ impl<'a> RegionWeeklyCommandHandler<'a> {
 
         let aggregate = self
             .aggregate_store
-            .load::<RegionalWeeklyReport>(&aggregate_id);
+            .load::<domain::RegionalWeeklyReport>(&aggregate_id);
         match aggregate.upload(command) {
             Ok(mut events) => self
                 .aggregate_store
@@ -59,24 +62,31 @@ impl<'a> RegionWeeklyCommandHandler<'a> {
 }
 
 pub struct RegionWeeklyUpload {
-    pub aggregate_id: AggregateId,
-    pub region: Region,
-    pub case: Case,
-    pub total_reported: TotalReported,
-    pub year_week: YearWeek,
+    pub aggregate_id: es::AggregateId,
+    pub region: vo::Region,
+    pub case: vo::Case,
+    pub total_reported: vo::TotalReported,
+    pub year_week: vo::YearWeek,
+}
+
+impl RegionWeeklyUpload {
+    pub fn new_id(region: vo::Region, year_week: vo::YearWeek) -> es::AggregateId {
+        es::AggregateId::from(&Uuid::new_v5(
+            &Uuid::from_str("a385bf4a-e6c0-48ee-a5e0-701e92f1e592").unwrap(),
+            format!("{}{}", region.name(), year_week).as_bytes(),
+        ))
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::super::vo;
     use super::*;
     use crate::common::clock::InMemoryClock;
     use crate::common::es;
     use crate::common::es::Stream;
+    use crate::domain::srag::vo;
     use chrono::DateTime;
-    use std::str::FromStr;
     use std::time::SystemTime;
-    use uuid::Uuid;
 
     #[test]
     fn it_uploads_a_new() {
@@ -84,13 +94,9 @@ mod tests {
         let clock = InMemoryClock::new(time);
         let mut stream = es::InMemoryStream::new(clock);
         let mut aggregate_store = es::AggregateStore::new(&mut stream);
-
-        let aggregate_id = es::AggregateId::from(&Uuid::new_v5(
-            &Uuid::from_str("a385bf4a-e6c0-48ee-a5e0-701e92f1e592").unwrap(),
-            format!("{}{}", vo::Region::Alagoas.name(), vo::YearWeek(2019, 10)).as_bytes(),
-        ));
-
         let mut handler = RegionWeeklyCommandHandler::new(&mut aggregate_store);
+
+        let aggregate_id = RegionWeeklyUpload::new_id(vo::Region::Alagoas, vo::YearWeek(2019, 10));
         let result = handler.handle(RegionWeeklyUpload {
             aggregate_id: aggregate_id.clone(),
             region: vo::Region::Alagoas,
